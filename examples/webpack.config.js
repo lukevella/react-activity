c6c464028ab922db75c95fadf8a16133e1e21ca9
Luke Vella <vellaluke@gmail.com>
2016-02-02 22:34:15 +0100

    Update examples to support gh-pages

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,15 +1,16 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
-  devtool: 'source-map',
+var config = {
+  devtool: 'cheap-module-eval-source-map',
   entry: {
     app: path.resolve('examples/components/index.js')
   },
   output: {
-    path: path.resolve('build'),
-    filename: '[name].js',
-    publicPath: 'build'
+    path: path.resolve('examples/build'),
+    filename: '[name].js'
   },
   module: {
     loaders: [
@@ -30,11 +31,16 @@ module.exports = {
   plugins: [
     new ExtractTextPlugin('style.css', {
       allChunks: true
+    }),
+    new HtmlPlugin({
+      filename: '../../index.html',
+      template: path.resolve('examples/index.html'),
+      inject: false
     })
   ],
   devServer: {
     colors: true,
-    contentBase: path.resolve(__dirname),
+    contentBase: path.resolve(__dirname + '/..'),
     historyApiFallback: true,
     hot: true,
     inline: true,
@@ -45,3 +51,15 @@ module.exports = {
     }
   },
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = 'source-map';
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {warnings: true},
+      output: {comments: false}
+    })
+  );
+}
+
+module.exports = config;
